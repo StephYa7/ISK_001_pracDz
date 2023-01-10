c5f4253a1e2030ca9e68049ff2bc12a5b718ed80
StephYa7 <1000p@mail.ru>
2023-01-10 19:11:03 +0700

    ex_3 hz

--- Prac_2/ex_02.java
+++ Prac_2/ex_02.java
@@ -1,13 +1,16 @@
-//package Prac_2;
-//
-//public class ex_02 {
-//
-//    try {
-//        int d = 0;
-//        double catchedRes1 = intArray[8] / d;
-//        System.out.println("catchedRes1 = " + catchedRes1);
-//    } catch (ArithmeticException e) {
-//        System.out.println("Catching exception: " + e);
-//    }
-//
-//}
+package Prac_2;
+
+public class ex_02 {
+
+    public static void main(String[] args){
+
+    try {
+        int[] intArray = {44,76,78,85,54,4,5,6,6,7};
+        int d = 0;
+        double catchedRes1 = intArray[8] / d;
+        System.out.println("catchedRes1 = " + catchedRes1);
+    } catch ( ArithmeticException e) {
+        System.out.println("Catching exception: " + e);
+    }
+
+}}
